8b455c0afec516624a43be98f1d22dfc24d58308
Yasumasa Ashida <ys.ashida@gmail.com>
2026-03-28 17:56:26 +0900

    fix(store): Fix nano/* in @vesperjs/vue

--- packages/vue/src/stores/nano/base-url.ts
+++ packages/vue/src/stores/nano/base-url.ts
@@ -1,3 +1,4 @@
 import { persistentAtom } from '@nanostores/persistent'
 
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-arguments
 export const $baseUrl = persistentAtom<string>('baseURL', '')
