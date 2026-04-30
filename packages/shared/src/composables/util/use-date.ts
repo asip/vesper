1d4a9abbd96fc015de8c8d6d1e27c2601b0506db
Yasumasa Ashida <ys.ashida@gmail.com>
2026-04-30 13:32:26 +0900

    refactor(composable): Refactor useDate with @formkit/tempo (#4)

--- packages/shared/src/composables/util/use-date.ts
+++ packages/shared/src/composables/util/use-date.ts
@@ -1,6 +1,13 @@
-export const useDate = function () {
-  const isValidDate = (str: string): boolean => {
-    return !!str && !!str.match(/\d{4}\/\d{2}\/\d{2}/) && !isNaN(Date.parse(str))
+import { parse } from '@formkit/tempo'
+
+export const useDate = function (fmtDate = 'YYYY/MM/DD', locale?: string) {
+  const isValidDate = (value: string): boolean => {
+    try {
+      const date = value ? parse(value, fmtDate, locale) : null
+      return date instanceof Date && !isNaN(date.getTime())
+    } catch {
+      return false
+    }
   }
 
   return { isValidDate }
