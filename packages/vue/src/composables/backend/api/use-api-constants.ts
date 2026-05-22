6d009c88ecb125a3e24a62df2e74985feeb39b22
Yasumasa Ashida <ys.ashida@gmail.com>
2026-05-23 07:25:35 +0900

    refactor(composable): Refactor use-api-constants in @vesperjs/vue

--- packages/vue/src/composables/backend/api/use-api-constants.ts
+++ packages/vue/src/composables/backend/api/use-api-constants.ts
@@ -2,7 +2,7 @@ import { WritableComputedRef } from '@vue/reactivity'
 import { useBaseUrlStore } from '~/stores/use-base-url-store'
 
 export const useApiConstants = function (): {
-  baseURL: WritableComputedRef<string, string>
+  baseURL: WritableComputedRef<string | undefined>
 } {
   const { baseURL } = useBaseUrlStore()
 
