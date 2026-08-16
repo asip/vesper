259c390b1c8049720121cc4cf3a6e162b0a0c45d
Yasumasa Ashida <ys.ashida@gmail.com>
2026-08-16 09:37:32 +0900

    refactor(composable): Refactor useElement in @vesperjs/vue

--- packages/vue/src/composables/ui/dom/use-element.ts
+++ packages/vue/src/composables/ui/dom/use-element.ts
@@ -3,7 +3,7 @@ import { computed, type Ref } from '@vue/reactivity'
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
 export const useElement = function <EL extends Element>(
   el: EL | undefined | null,
-  { property }: { property: string },
+  { property, properties }: { property?: string; properties?: string[] },
 ): Record<string, Ref<string, string | null | undefined>> {
   const obj: Partial<Record<string, Ref<string, string | null | undefined>>> = {}
 
@@ -11,41 +11,51 @@ export const useElement = function <EL extends Element>(
     return str.toLowerCase().replace(/(-[a-z])/g, (group) => group.toUpperCase().replace('-', ''))
   }
 
-  const dataAttrProp = property.startsWith('data-')
-    ? toLowerCamel(property.replace('data-', ''))
-    : undefined
+  const addPropertyRef = (prop: string) => {
+    const dataAttrProp = prop.startsWith('data-')
+      ? toLowerCamel(prop.replace('data-', ''))
+      : undefined
 
-  const propertyRef = computed<string, string | null | undefined>({
-    get() {
-      if (dataAttrProp) {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        return el && 'dataset' in el && dataAttrProp in (el.dataset as any) // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
-          ? ((el.dataset as any)[dataAttrProp] as string)
-          : ''
-      } else {
-        return el && property in el
-          ? // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
-            ((el as any)[property] as string)
-          : ''
-      }
-    },
-    set(value: string | null | undefined) {
-      if (dataAttrProp) {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        if (el && 'dataset' in el && dataAttrProp in (el.dataset as any)) {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
-          ;(el.dataset as any)[dataAttrProp] = value ?? ''
+    const propertyRef = computed<string, string | null | undefined>({
+      get() {
+        if (dataAttrProp) {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          return el && 'dataset' in el && dataAttrProp in (el.dataset as any) // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+            ? ((el.dataset as any)[dataAttrProp] as string)
+            : ''
+        } else {
+          return el && prop in el
+            ? // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+              ((el as any)[prop] as string)
+            : ''
         }
-      } else {
-        if (el && property in el) {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
-          ;(el as any)[property] = value ?? ''
+      },
+      set(value: string | null | undefined) {
+        if (dataAttrProp) {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          if (el && 'dataset' in el && dataAttrProp in (el.dataset as any)) {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+            ;(el.dataset as any)[dataAttrProp] = value ?? ''
+          }
+        } else {
+          if (el && prop in el) {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+            ;(el as any)[prop] = value ?? ''
+          }
         }
-      }
-    },
-  })
+      },
+    })
 
-  obj[property] = propertyRef
+    obj[prop] = propertyRef
+  }
+
+  if (property) {
+    addPropertyRef(property)
+  } else if (properties && properties.length > 0) {
+    properties.forEach((prop) => {
+      addPropertyRef(prop)
+    })
+  }
 
   return obj as Record<string, Ref<string, string | null | undefined>>
 }
