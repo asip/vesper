1fbef551b204d512a19ca38e6075036d857c408e
Yasumasa Ashida <ys.ashida@gmail.com>
2026-09-07 15:52:48 +0900

    refactor(composable): Refactor useRecord in @vesperjs/vue

--- packages/vue/src/composables/use-record.ts
+++ packages/vue/src/composables/use-record.ts
@@ -1,6 +1,9 @@
+import { WritableComputedRef } from '@vue/reactivity'
 import { useRecordStore } from '~/stores'
 
-export const useRecord = function (key: string) {
+export const useRecord = function (key: string): {
+  record: WritableComputedRef<Partial<Record<string, string>>>
+} {
   const { record } = useRecordStore(key)
 
   return { record }
